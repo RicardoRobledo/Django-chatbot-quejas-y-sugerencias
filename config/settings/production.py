eb1b4eef08c07cd65aea226ee146b092ee6e7a36
Ricardo Robledo <ricardoprogrammer44@gmail.com>
2024-07-31 20:32:39 -0600

    feat(add): production commit

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,14 +3,22 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'django-chatbot-quejas-y-sugerencias.onrender.com']
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000  # 1 año
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
+                 'django-chatbot-quejas-y-sugerencias.onrender.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 STATIC_ROOT = BASE_DIR/'assets'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
